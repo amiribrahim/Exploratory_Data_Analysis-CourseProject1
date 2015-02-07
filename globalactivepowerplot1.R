05bd166ef48dbdeae3f6e5cedc1ace2385622e12
Fighterpilot87 <Fighterpilot87@gmail.com>
2015-02-07 12:00:33 -0500

    Updated time

--- globalactivepowerplot1.R
+++ globalactivepowerplot1.R
@@ -3,14 +3,18 @@ hpcfile <- read.table(
   sep = ";", header = TRUE)
 
 hpc <- hpcfile[hpcfile$Date == "1/2/2007" | hpcfile$Date == "2/2/2007", ]
-hpc$Date <- as.Date(hpc$Date, format = "%d/%m/%Y")
-hpc$Time <- strptime(hpc$Time, format = "%H:%M:%S")
+hpc$DT <- paste(hpc$Date, hpc$Time)
+hpc[, 1] <- hpc$DT
+hpc <- subset(hpc, select = -c(DT, Time))
+hpc$Date <- strptime(hpc$Date, format = "%d/%m/%Y %H:%M:%S")
 
-for(i in 3:ncol(hpc)){
+for(i in 2:ncol(hpc)){
   hpc[,i] <- as.numeric(levels(hpc[,i])[hpc[,i]])
 }
 
 hist(hpc$Global_active_power, col = "red", 
      xlab = "Global Active Power", 
      main = "Global Active Power")
+
 dev.copy(png, file = "GlobalActivePower.png")
+dev.off()
