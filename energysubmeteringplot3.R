bf90a849d68e10cabe27f6da72fe0111e8d5463f
Fighterpilot87 <Fighterpilot87@gmail.com>
2015-02-08 03:50:22 -0500

    Plot 4

--- energysubmeteringplot3.R
+++ energysubmeteringplot3.R
@@ -7,6 +7,11 @@ hpc$DT <- paste(hpc$Date, hpc$Time)
 hpc[, 1] <- hpc$DT
 hpc <- subset(hpc, select = -c(DT, Time))
 hpc$Date <- strptime(hpc$Date, format = "%d/%m/%Y %H:%M:%S")
+
+#Convert factor values to numeric
+for(i in c(2, 3, 4, 6, 7)){
+  hpc[,i] <- as.numeric(levels(hpc[,i])[hpc[,i]])
+}
 hpc[is.na(hpc)] <- 0
 
 plot(hpc$Date, hpc$Sub_metering_1, type = "l", yaxt = "n",
@@ -14,8 +19,9 @@ plot(hpc$Date, hpc$Sub_metering_1, type = "l", yaxt = "n",
 axis(2, at = seq(0, 30, by = 10), las = 2)
 lines(hpc$Date, hpc$Sub_metering_2, col = "red")
 lines(hpc$Date, hpc$Sub_metering_3, col = "blue")
-legend("topright", pch = "_", col = c("black", "red", "blue"), 
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty = 1, col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       cex = .75)
 
 
 dev.copy(png, file = "Plot3.png")
